4bdbfcbcbfe95b3db9c8447ab484648cfea94dad
Daniel K <kantordani24@gmail.com>
2019-09-24 18:51:56 +0200

    Update csere.cpp

--- Labor-2/csere.cpp
+++ Labor-2/csere.cpp
@@ -4,32 +4,34 @@ using namespace std;
 
 int main()
 {
-    int a = 10;
-    int b = 13;
+    int a;
+    int b;
     
     // csere kulombseggel es osszeggel
-
+    cout << "Adjon meg ket szamot: " << endl;
+    cin >> a >> b;
+    
     a = a + b;
     b = a - b;
     a = a - b;
-
+    
     cout << "Valtozok cserelve a kulombseggel: \nElso: " << a << "\nMasodik: " << b << endl;
-
+    
     // csere szorzattal
-
+    
     a = a * b;
     b = a / b;
     a = a / b;
-
+    
     cout << "Valtozok cserelve szorzattal: \nElso: " << a << "\nMasodik: " << b << endl;
-
+    
     // csere xorral
-
+    
     a = a xor b;
     b = a xor b;
     a = a xor b;
-
-    cout << "Valtozok cserelve xorral: \nElso: " << A << "\nMasodik: " << b << endl; 
-
+    
+    cout << "Valtozok cserelve xorral: \nElso: " << a << "\nMasodik: " << b << endl;
+    
     return 0;
 }
